e6e1f079cd4d8c5f3026155b091223f3f788d480
Rishit21-oss <81700717+Rishit21-oss@users.noreply.github.com>
2022-01-11 20:34:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 function preload(){
+    clown_nose = loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png');
 
 }
 
@@ -17,16 +18,22 @@ function modelLoaded(){
     console.log("PoseNet is Initialized");
 }
 
+noseX= 0;
+noseY= 0;
+
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
-        console.log("nose x ="+ results[0].pose.nose.x);
-        console.log("nose y ="+ results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x-15;
+        noseY = results[0].pose.nose.y-15;
+        console.log("noseX =" + noseX);
+        console.log("noseY =" + noseY);
     }
 }
 
 function draw(){
  image(video , 0 , 0 , 300 , 300);
+ image(clown_nose, noseX, noseY, 30, 30);
 }
 
 function take_snapshot(){
